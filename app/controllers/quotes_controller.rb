68d3c3d112c2e256e3017246dcb534f8bdb3800e
Jean Chavez <diegojeanpier.dc@gmail.com>
2024-02-17 21:02:01 -0500

    feat: chapter 4 turbo diving Update quotes controller and views

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
   def index
-    @quotes = Quote.all
+    @quotes = Quote.ordered
   end
 
   def show
@@ -16,7 +16,10 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -35,7 +38,27 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream
+    end
+
+    # # Remove a Turbo Frame
+    # turbo_stream.remove
+
+    # # Insert a Turbo Frame at the beginning/end of a list
+    # turbo_stream.append
+    # turbo_stream.prepend
+
+    # # Insert a Turbo Frame before/after another Turbo Frame
+    # turbo_stream.before
+    # turbo_stream.after
+
+    # # Replace or update the content of a Turbo Frame
+    # turbo_stream.update
+    # turbo_stream.replace
+
   end
 
   private
